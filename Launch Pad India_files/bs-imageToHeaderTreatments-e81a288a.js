61f79cdda23386a633b498875b610e7b44c579b5
Abhemanyu Sarin <abhemanyus@gmail.com>
2024-09-20 12:46:44 +0530

    use template for better design

--- Launch Pad India_files/bs-imageToHeaderTreatments-e81a288a.js
+++
@@ -1,2 +0,0 @@
-define("@widget/LAYOUT/c/bs-imageToHeaderTreatments-e81a288a.js",["exports","~/c/bs-overlayTypes"],(function(e,i){"use strict";e.i=function(e){return Object.keys(e).filter((e=>e!==i.L))}})),"undefined"!=typeof window&&(window.global=window);
-//# sourceMappingURL=bs-imageToHeaderTreatments-e81a288a.js.map
